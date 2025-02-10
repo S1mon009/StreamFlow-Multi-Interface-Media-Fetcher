c23ad4e45e1971f6de62cbc43f1c38243bc845b2
S1mon009 <skaszy25@gmail.com>
2025-02-10 18:16:28 +0100

    Modify main file

--- main.py
+++ main.py
@@ -1,80 +1,77 @@
 import os
 import subprocess
-import textwrap
-from pprint import pprint
-# import shutil
-# from flask import Flask, request, jsonify, send_file
 from yt_dlp import YoutubeDL
-# from flask_cors import CORS
+import inquirer
+
+options = [
+    inquirer.List('quality',
+                  message="Choose quality",
+                  choices=['The best', 'Medium', 'High', 'Low']),
+    inquirer.List('output_format',
+                  message="Choose output format",
+                  choices=['Mkv', 'Mp4'])
+]
+
+quality_list = {
+    'The best': 'bestvideo+bestaudio/best',
+    'Medium': 'bestvideo[height<=1440]+bestaudio/best[height<=1440]',
+    'High': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
+    'Low': 'bestvideo[height<=480]+bestaudio/best[height<=480]'
+}
 
 DOWNLOAD_FOLDER = 'E:/Wideo'
 
-def display_videos(url):
-    ydl_opts = {
-        'quiet': True,  # Wyłącza zbędne logi
-        'extract_flat': True,  # Pobiera listę linków bez pobierania treści
-        'skip_download': True
-    }
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
     
-    
-    try:
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)  # Pobierz informacje bez pobierania
+def check_is_playlist(url):
+    """
+    Prosta funkcja sprawdzająca, czy link zawiera parametr list=, 
+    co zazwyczaj wskazuje, że jest to playlista.
+    """
+    return 'list=' in url
 
-            if 'original_url' in info:
-                print("playlista")
-            else:
-                print("wideo")
-            # pprint.pprint(info)
-    except Exception as e:
-        print(f"Nie można pobrać informacji o filmie: {e}")
-        
-def download_video(url):
-    output_path = os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s')
-    options = {
-        'format': 'bestvideo+bestaudio/best',  # Pobiera najlepsze wideo i audio, a następnie je łączy
-        'merge_output_format': 'mkv',         # Format wyjściowy po połączeniu
-        'outtmpl': f'{output_path}/%(title)s.%(ext)s',  # Ścieżka i nazwa pliku
-        'noplaylist': False,                   # Nie pobiera playlist, tylko pojedyncze wideo
-        'quiet': False,                       # Pokazuje szczegóły pobierania
-        'postprocessors': [{                  # Dodatkowe przetwarzanie po pobraniu
+def download_video(url, options):
+    is_playlist = check_is_playlist(url)
+    
+    # Jeśli link dotyczy playlisty, zapytaj o nazwę folderu i utwórz go
+    if is_playlist:
+        playlist_folder_name = input("Enter the name of the playlist folder: ")
+        playlist_folder_path = os.path.join(DOWNLOAD_FOLDER, playlist_folder_name)
+        if not os.path.exists(playlist_folder_path):
+            os.makedirs(playlist_folder_path)
+        output_path = os.path.join(playlist_folder_path, '%(title)s.%(ext)s')
+    else:
+        output_path = os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s')
+    
+    download_options = {
+        'format': quality_list[options['quality']],
+        'merge_output_format': options['output_format'].lower(),
+        'outtmpl': output_path,
+        # Jeśli nie jest playlistą, ustaw noplaylist na True
+        'noplaylist': not is_playlist,
+        'quiet': False,
+        'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
-            'preferedformat': 'mkv'          # Konwersja do formatu MP4
+            'preferedformat': options['output_format'].lower()
         }]
     }
-    try:
-        with YoutubeDL(options) as ydl:
-            info = ydl.extract_info(url, download=False)
-            
-            if 'entries' in info:
-                for i in info['entries']:
-                    print(f"{i['title']}:")
-                    formats = i['formats']
-                    for f in formats:
-                        resolution = f.get('height', 'Audio')  # Wysokość wideo (lub Audio)
-                        ext = f.get('ext', 'unknown')  # Format pliku
-                        fps = f.get('fps', '-')  # Liczba klatek na sekundę
-                        size = f.get('filesize', None)  # Rozmiar pliku
-                        size_mb = f"{size / 1024 / 1024:.2f} MB" if size else "N/A"
-
-                        print(f"- {f['format_id']:>4}: {resolution}p, {ext}, {fps} fps, {size_mb}")
-
-                print("playlista")
-            else:
-                print("wideo")
-            # ydl.download([url])
-    except Exception as e:
-        return str(e)
+    
+    with YoutubeDL(download_options) as ydl:
+        ydl.download([url])
+    print('Download completed')
 
 if __name__ == "__main__":
+    clear_console()
     try:
-        result = subprocess.run(['ffmpeg', '-version'], capture_output=True, text=True)
-    
+        result = subprocess.run(['ffmpeg', '-version'], capture_output=True, text=True, check=True) 
         if result.returncode == 0:
+            # Upewnij się, że domyślny folder do pobierania istnieje
             if not os.path.exists(DOWNLOAD_FOLDER):
                 os.makedirs(DOWNLOAD_FOLDER)
-            url = input("Podaj URL filmu z YouTube: ")
-            download_video(url)
+            url = input("Enter the url from YouTube: ")
+            selected_options = inquirer.prompt(options)
+            download_video(url, selected_options)
         else:
             print('ffmpeg is not installed')
     except FileNotFoundError:
